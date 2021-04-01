6bc4c18c18dd7415ba9cccc956d3119a55cd1a8e
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-04-01 22:30:31 +0300

    added gsap and added header and footer medialeaks

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+import gsap from 'gsap';
 
 export default function Start() {
+
+    let start = useRef(null);
+
+    useEffect(() => {
+        gsap.to(start, {
+            duration: 0.8,
+            opacity: 1,
+            ease: "power3.inOut"
+        });
+    }, []);
+
     return (
-        <div>
+        <div className="start" ref={el => (start = el)}>
             Start
         </div>
     )
