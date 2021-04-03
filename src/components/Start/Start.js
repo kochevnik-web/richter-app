6318e961148389e687d1b8c7d9b50fd940a87adf
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-04-03 12:17:29 +0300

    check animation path

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,5 +1,6 @@
 import React, {useRef, useEffect} from 'react';
 import gsap from 'gsap';
+import MotionPathPlugin from 'gsap/MotionPathPlugin'
 
 import logo from './img/logo.svg';
 
@@ -12,7 +13,10 @@ import flower_15 from './img/flower-15.svg';
 
 export default function Start() {
 
+    gsap.registerPlugin(MotionPathPlugin)
+
     let start = useRef(null);
+    let path = useRef(null);
     let subTitle = useRef(null);
     let title = useRef(null);
     let fl_1 = useRef(null);
@@ -25,12 +29,25 @@ export default function Start() {
             opacity: 1,
             ease: "power3.inOut"
         });
+        // gsap.to(fl_1, {
+        //     rotation:"360",
+        //     duration: 35,
+        //     ease: "linear",
+        //     repeat: -1
+        // });
         gsap.to(fl_1, {
-            rotation:"360",
-            duration: 35,
-            ease: "linear",
-            repeat: -1
-        });
+            duration: 5, 
+            repeat: 12,
+            repeatDelay: 3,
+            yoyo: true,
+            ease: "power1.inOut",
+            motionPath:{
+              path: path,
+              align: path,
+              autoRotate: true,
+              alignOrigin: [0.5, 0.5]
+            }
+          });
         gsap.to(fl_3, {
             rotation:"-=360",
             duration: 35,
@@ -100,6 +117,15 @@ export default function Start() {
                     <img className="start-flower" src={flower_15} alt="Гедеон Рихтер" ref={el => (fl_15 = el)} />
                     {flowersList}
                 </div>
+                <svg width="100%" height="100%" viewBox="-20 0 557 190" id="svg">
+  <circle cx="100" cy="100" r="3" />
+  <circle cx="300" cy="20" r="3" />
+  <path ref={el => (path = el)} d="M9,100c0,0,18.53-41.58,49.91-65.11c30-22.5,65.81-24.88,77.39-24.88c33.87,0,57.55,11.71,77.05,28.47c23.09,19.85,40.33,46.79,61.71,69.77c24.09,25.89,53.44,46.75,102.37,46.75c22.23,0,40.62-2.83,55.84-7.43c27.97-8.45,44.21-22.88,54.78-36.7c14.35-18.75,16.43-36.37,16.43-36.37"/>
+  <g id="rect">
+    <rect width="85" height="30" fill="dodgerblue" />
+    <text x="10" y="19" font-size="14">SVG &lt;rect&gt;</text>
+  </g>
+</svg>
             </div>
         </div>
     )
