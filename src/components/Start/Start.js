6a507b9e6876ef19aee95ab1d854f59b73dc33bd
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-04-03 17:40:09 +0300

    added 2 flower animation and finish desctop start

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -11,6 +11,7 @@ import flower_1 from './img/flower-1.svg';
 import flower_3 from './img/flower-3.svg';
 import flower_15 from './img/flower-15.svg';
 import flower_16 from './img/flower-16.svg';
+import flower_17 from './img/flower-17.svg';
 
 export default function Start() {
 
@@ -22,16 +23,18 @@ export default function Start() {
 
     let start = useRef(null);
     let path = useRef(null);
+    let path_2 = useRef(null);
     let fl_1 = useRef(null);
     let fl_3 = useRef(null);
     let fl_15 = useRef(null);
     let fl_16 = useRef(null);
+    let fl_17 = useRef(null);
 
     const handleStartGame = () => {
         setStartGame(true);
     }
 
-    //animation flowars and text
+    //animation flowars
     useEffect(() => {
         gsap.to(start, {
             duration: 1.2,
@@ -59,14 +62,15 @@ export default function Start() {
 
     }, []);
 
+    //animation for texts
     useEffect(() => {
         setTimeout(() => {
             setFide_1(true);
-        }, 300);
+        }, 500);
 
         setTimeout(() => {
             setFide_2(true);
-        }, 500);
+        }, 700);
     },[]);
 
     //animation flowers for button click
@@ -83,6 +87,22 @@ export default function Start() {
                 }
             });
             gsap.to(fl_16, {
+                delay: 1,
+                duration: 1,
+                opacity: 0,
+                ease: "liner"
+            });
+            gsap.to(fl_17, {
+                duration: 2,
+                scale: 1.8,
+                ease: "power2.out",
+                motionPath:{
+                  path: path_2,
+                  align: path_2,
+                  alignOrigin: [0.5, 0.5]
+                }
+            });
+            gsap.to(fl_17, {
                 delay: 1,
                 duration: 1,
                 opacity: 0,
@@ -137,11 +157,18 @@ export default function Start() {
                     <img className="start-flower" src={flower_3} alt="Гедеон Рихтер" ref={el => (fl_3 = el)} />
                     <img className="start-flower" src={flower_15} alt="Гедеон Рихтер" ref={el => (fl_15 = el)} />
                     <img className="start-flower" src={flower_16} alt="Гедеон Рихтер" ref={el => (fl_16 = el)} />
+                    <img className="start-flower" src={flower_17} alt="Гедеон Рихтер" ref={el => (fl_17 = el)} />
+
                     {flowersList}
+
+                    <svg viewBox="-20 0 557 190" id="path-1" className="path-move">
+                        <path ref={el => (path = el)} d="M9,100c0,0,18.53-41.58,49.91-65.11c30-22.5,65.81-24.88,77.39-24.88c33.87,0,57.55,11.71,77.05,28.47c23.09,19.85,40.33,46.79,61.71,69.77c24.09,25.89,53.44,46.75,102.37,46.75c22.23,0,40.62-2.83,55.84-7.43c27.97-8.45,44.21-22.88,54.78-36.7c14.35-18.75,16.43-36.37,16.43-36.37"/>
+                    </svg>
+
+                    <svg viewBox="-20 0 557 190" id="path-2" className="path-move">
+                        <path ref={el => (path_2 = el)} d="M9,100c0,0,18.53-41.58,49.91-65.11c30-22.5,65.81-24.88,77.39-24.88c33.87,0,57.55,11.71,77.05,28.47c23.09,19.85,40.33,46.79,61.71,69.77c24.09,25.89,53.44,46.75,102.37,46.75c22.23,0,40.62-2.83,55.84-7.43c27.97-8.45,44.21-22.88,54.78-36.7c14.35-18.75,16.43-36.37,16.43-36.37"/>
+                    </svg>
                 </div>
-                <svg width="100%" height="100%" viewBox="-20 0 557 190" id="path-1" className="path-move">
-                    <path ref={el => (path = el)} d="M9,100c0,0,18.53-41.58,49.91-65.11c30-22.5,65.81-24.88,77.39-24.88c33.87,0,57.55,11.71,77.05,28.47c23.09,19.85,40.33,46.79,61.71,69.77c24.09,25.89,53.44,46.75,102.37,46.75c22.23,0,40.62-2.83,55.84-7.43c27.97-8.45,44.21-22.88,54.78-36.7c14.35-18.75,16.43-36.37,16.43-36.37"/>
-                </svg>
             </div>
         </div>
     )
