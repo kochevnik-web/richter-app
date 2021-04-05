4cd52360df50dae5433a5e60906207480b5433d4
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-04-05 17:21:09 +0300

    change useScroll

--- src/scroll.js
+++ src/scroll.js
@@ -2,12 +2,12 @@ import React, { useRef } from "react";
 
 const useScroll = () => {
   const elRef = useRef(null);
-  const executeScroll = () =>
-    elRef.current.scrollIntoView({
-      behavior: "smooth",
-      block: "start"
-    });
-  //const executeScroll = () => window.scrollTo(0, elRef.current.offsetTop);
+  // const executeScroll = () =>
+  //   elRef.current.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "start"
+  //   });
+  const executeScroll = () => window.scrollTo({top: elRef.current.offsetTop, behavior: 'smooth'});
   return [executeScroll, elRef];
 };
 
