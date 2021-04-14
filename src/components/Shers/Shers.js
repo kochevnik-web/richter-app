8c9a755a6cec5f3a18d51b3d623c2b9abc95c0c0
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-04-14 12:18:54 +0300

    Remove animation for Shers animation

--- src/components/Shers/Shers.js
+++ src/components/Shers/Shers.js
@@ -1,47 +1,22 @@
-import React, {useRef, useEffect} from 'react';
+import React from 'react';
 import {FacebookShareButton,TwitterShareButton,VKShareButton} from "react-share";
 
 import data from './shers.data';
 
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-
 export default function Shers() {
-
-    gsap.registerPlugin(ScrollTrigger);
 
     const title = 'Спецпроект. Как устроены женщины?';
     // const desc  = 'Женщины устроены довольно сложно — это мы сейчас не о внутреннем мире, а о физиологии. Вместе с компанией «Гедеон Рихтер» предлагаем вам проверить свои знания на эту тему!';
     const url   = 'https://medialeaks.ru/what-are-girls-made-of/';
     // "homepage": "https://medialeaks.ru/what-are-girls-made-of/",
 
-    let elRef = useRef([]);
     let imgShare = process.env.PUBLIC_URL+ '/rech-app.pmg';
-
-    useEffect(() => {
-        gsap.fromTo(elRef.current, {
-            y: 50
-          }, {
-            delay: 0.5,
-            duration: 1,
-            ease: 'back',
-            scrollTrigger: {
-              trigger: elRef.current,
-              start: 'top center+=100',
-            },
-            y: 0,
-            stagger: {
-                amount: -0.5,
-            }
-          });
-    }, []);
 
     return (
         <div className="Shers">
-            {data.map((el, indx) => {
+            {data.map((el) => {
                 return (
                     <span
-                        ref={el => {elRef.current[indx] = el}}
                         key={el.id}
                         className={"share-btn share-btn-" + el.id}
                     >
